ba7f32b5b4647f75358b248839d2eba1e34dd6f3
Matthew Simo <matthew.a.simo@gmail.com>
2023-06-29 11:54:23 -0500

    rename some thigns

--- projects/01 WeSplit/WeSplit/ContentView.swift
+++ projects/01 WeSplit/WeSplit/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
+            
                 Section {
                     TextField("Amount", value: $checkAmount, format: asCurrency)
                         .keyboardType(.decimalPad)

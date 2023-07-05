f65a203bce16eb0984a9f0f6569f6725028c8ee2
Matthew Simo <matthew.a.simo@gmail.com>
2023-07-05 14:58:12 -0500

    D31 - Project 04: WordScramble

---
+++ projects/04 WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Matthew Simo on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

6bc1d628e1055d34e6f6527c542d165477a0219a
Matthew Simo <matthew.a.simo@gmail.com>
2023-07-07 12:21:44 -0500

    D37. Project 05 iExpense

---
+++ projects/05 iExpense/iExpense/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Matthew Simo on 7/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationView {
+            
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        
+                        VStack(alignment: .leading){
+                            
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: "USD"))
+                        
+                    }
+                    
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+            
+        }
+    }
+    
+    func removeItems(at offset: IndexSet) {
+        expenses.items.remove(atOffsets: offset)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projects/05 iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Matthew Simo on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

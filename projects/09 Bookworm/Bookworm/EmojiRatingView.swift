ac0ff80e768c61e5bac326b68714bb2900a997d1
Matthew Simo <matthew.a.simo@gmail.com>
2023-07-14 15:54:45 -0500

    D54. Project 09 Bookworm p2

---
+++ projects/09 Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Matthew Simo on 7/14/23.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    
+    let rating: Int16
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("🙊")
+        case 2:
+            return Text("🙁")
+        case 3:
+            return Text("😐")
+        case 4:
+            return Text("😄")
+        default:
+            return Text("🤩")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
---
+++ projects/09 Bookworm/Bookworm/RatingView.swift
@@ -0,0 +1,49 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Matthew Simo on 7/14/23.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximiumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maximiumRating + 1, id: \.self) { number in
+                image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
+                    .onTapGesture {
+                        rating = number
+                    }
+            }
+        }
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: .constant(4))
+}

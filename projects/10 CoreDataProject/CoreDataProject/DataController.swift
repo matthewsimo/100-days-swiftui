27ede537d534b76c384cf454c4bb2bcf23c591be
Matthew Simo <matthew.a.simo@gmail.com>
2023-07-17 14:24:39 -0500

    D57. Project 10 CoreDataProject p1

---
+++ projects/10 CoreDataProject/CoreDataProject/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Matthew Simo on 7/17/23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+            
+        }
+    }
+}
